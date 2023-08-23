ec72d651074bf143cda64ee2d84d9de803c23001
Thorinka <mmlentyshenko@gmail.com>
2023-08-23 21:31:58 +0300

    init + models added

---
+++ mailing/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+# Create your models here.
+
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name='email')
+    client_name = models.CharField(max_length=150, verbose_name='фио')
+    comment = models.TextField(verbose_name='комментарий')
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name ='клиент'
+        verbose_name_plural = 'клиенты'
+
+class MailingMessage(models.Model):
+    subject = models.CharField(max_length=50, verbose_name='тема')
+    text = models.TextField(verbose_name='текст')
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'cooбщения'
+
+
+class MailSettings(models.Model):
+    PERIOD_DAILY = 'daily'
+    PERIOD_WEEKLY = 'weekly'
+    PERIOD_MONTHLY = 'monthly'
+
+    PERIODS = (
+        (PERIOD_DAILY, 'Ежедневно'),
+        (PERIOD_WEEKLY, 'Еженедельно'),
+        (PERIOD_MONTHLY, 'Ежемесячно'),
+    )
+
+    STATUS_CREATED = 'created'
+    STATUS_IN_PROCESS = 'in_process'
+    STATUS_FINISHED = 'finished'
+
+    STATUSES = (
+        (STATUS_CREATED, 'Создано'),
+        (STATUS_IN_PROCESS, 'В процессе'),
+        (STATUS_FINISHED, 'Завершена'),
+    )
+
+    mailing_time = models.DateTimeField(verbose_name='время')
+    period = models.CharField(max_length=20, choices=PERIODS, default=PERIOD_DAILY, verbose_name='период')
+    status = models.CharField(max_length=20, choices=STATUSES, default=STATUS_CREATED, verbose_name='период')
+
+    message = models.ForeignKey(MailingMessage, on_delete=models.CASCADE, verbose_name='сообщение')
+    def __str__(self):
+        return f'{self.mailing_time} / {self.period}'
+
+    class Meta:
+        verbose_name = 'настройка'
+        verbose_name_plural = 'настройки'
+
+
+class MailingClient(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='клиент')
+    settings = models.ForeignKey(MailSettings, on_delete=models.CASCADE, verbose_name='настройки')
+
+    def __str__(self):
+        return f'{self.client} / {self.settings}'
+
+    class Meta:
+        verbose_name = 'клиент рассылки'
+        verbose_name_plural = 'клиенты рассылки'
+
+
+class MailingLog(models.Model):
+    STATUS_SUCCESS = 'success'
+    STATUS_FAILED = 'failed'
+    STATUSES = (
+        (STATUS_SUCCESS, 'Успешно'),
+        (STATUS_FAILED, 'Ошибка'),
+    )
+
+    last_try = models.DateTimeField(auto_now_add=True, verbose_name='дата последней попытки')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='клиент')
+    settings = models.ForeignKey(MailSettings, on_delete=models.CASCADE, verbose_name='настройки')
+    status = models.CharField(choices=STATUSES, default=STATUS_SUCCESS, verbose_name='статус')
+
+    def __str__(self):
+        return f'{self.last_try}'
+
+    class Meta:
+        verbose_name = 'лог'
+        verbose_name_plural = 'логи'
+
+
---
+++ mailing/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from mailing.apps import MailingConfig
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+    # path(),
+]
